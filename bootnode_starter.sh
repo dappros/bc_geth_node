d337a47d00fff5d8af76cd40324b184ab277e088
Taras <phwizard@gmail.com>
2021-07-21 13:07:20 +0100

    boot node starter created

---
+++ bootnode_starter.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Geth private blockchain bootnode starter script (c) Taras Filatov, Dappros Ltd, 21.07.2021
+# This restarts bootnode service automatically to help nodes discover each other
+#
+# First time launch on a new node (to generate your enode address key):
+# bootnode -genkey bootnode.key
+#
+# All consequent launches - recommended start routine:
+# (1) Enter 'screen';
+# (2) Execute 'sh bootnode_starter.sh | tee -a >> bootnode_starter.screen.log';
+# (3) Press 'Ctrl+a' and then 'd' to detach from screen session
+# You can always re-attach screen session via 'screen-r' or you can check logs
+
+while ! bootnode -nodekey boot.key -verbosity 9 -addr :30310
+do
+  sleep 1
+  echo "Restarting bootnode service following some termination.."
+  echo $(date -u) "Bootnode service restarted following some termination.." >> bootnode_starter.sh.log
+done
